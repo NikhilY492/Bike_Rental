88e6133cbccce3b80442af268320cf468d07c09b
Sai Nikhil Y <147718705+NikhilY492@users.noreply.github.com>
2024-05-09 01:56:33 +0530

    Update script.js

--- proj/script.js
+++ proj/script.js
@@ -5,4 +5,29 @@ document.querySelector('#close-login-form').onclick=() =>{
     document.querySelector('.login-form-container').classList.remove('active')
 }
 
-
+var swiper = new Swiper (".featured-slider",{
+slidesPerView: 1,
+spaceBetween: 20,
+loop:true,
+grabCursor:true,
+centeredSlides:true,
+autoplay: {
+delay: 9500,
+disableOnInteraction: false,
+},
+pagination: {
+el: ".swiper-pagination",
+clickable: true,
+},
+breakpoints: {
+0: {
+slidesPerView: 1,
+},
+768: {
+slidesPerView: 2,
+},
+1040:{
+    slidesPerView:3,
+},
+},
+});
