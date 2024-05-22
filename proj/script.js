d162b436b7eb12b5c5e23b635a83c3c774f660db
NikhilY492 <492nikhil@gmail.com>
2024-05-23 05:15:39 +0530

    Payment page and slider
    Organized files a bit

--- proj/script.js
+++ proj/script.js
@@ -18,3 +18,47 @@ document.querySelector('#close-sign-form').onclick=() =>{
     document.querySelector('.sign-form-container').classList.remove('active')
     document.querySelector('.login-form-container').classList.remove('active')
 }
+
+//swiper starts here//
+
+var swiper = new Swiper (".featured-slider",{
+    loop:true,
+    grabCursor:true,
+    centeredSlides:true,
+    autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      keyboard: {
+        enabled: true,
+      },
+    breakpoints: {
+    0: {
+    slidesPerView: 1,
+    },
+    768: {
+    slidesPerView: 2,
+    },
+    1040:{
+        slidesPerView:4,
+        spaceBetween: 30,
+        freeMode: true,
+    },
+    
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+    });
+
+
+
